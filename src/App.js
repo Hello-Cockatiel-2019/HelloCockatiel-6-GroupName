a9fb728102e781aa9cf4d09cf5e56a775d626ed4
mukpch <pacharaporn.ksn@mail.kmutt.ac.th>
2019-11-04 18:33:40 +0700

    ปุ่มcontent(ยังไม่เสร็จ)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import ContentLists from './components/ContentLists/';
 import './style.css';
 import Buttons from './components/Buttons/';
+
 function App() {
   return (
     <div className="App">
        {/* Leopard */}
       <h1>เสือรู้สึกเเย่ เสือเสียใจ เสือจะร้องไห้ เพราะเสือdown</h1> 
        <ContentLists/>
+       
        <Buttons/>
+       
     </div>
   );
 };
--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,21 +1,47 @@
 import React, { Component } from "react";
+import styled from 'styled-components';
 export default class Buttons extends Component {
 
     render() {
 
         return (
-
             <div>
-                
-                
-                <div className="con-container">
-                <span className="content-btn"><img src="/static/images/content02.png" /></span>
-                <span className="content-btn2"><img src="/static/images/content-b02.png" /></span>
 
-                </div>
+                    <div>
+                    <div className="con-container">
+                    <span className="content1-btn"><img src="/static/images/content-b01.png"  width="40%" height="80%"/></span>
+                    <span className="content1-btn2"><img src="/static/images/content.png" width="40%" height="80%"/></span>
+                    </div>
+                    
+                    <div className="con2-container">
+                    <span className="content2-btn"><img src="/static/images/content-b02.png" width="40%" height="80%"/></span>
+                    <span className="content2-btn2"><img src="/static/images/content02.png" width="40%" height="80%"/></span>
+                    </div>
+                    
+                    
+                    
+                    <div className="con3-container">      
+                    <span className="content3-btn"><img src="/static/images/content-b03.png" width="40%" height="80%"/></span>
+                    <span className="content3-btn2"><img src="/static/images/content03.png" width="40%" height="80%"/></span>
+                    </div>
+                    
+                    <div className="con4-container">      
+                    <span className="content4-btn"><img src="/static/images/content-b04.png" width="40%" height="80%"/></span>
+                    <span className="content4-btn2"><img src="/static/images/content04.png" width="40%" height="80%"/></span>
+                    </div>
 
-                
-                </div>
+                    <div className="con5-container">      
+                    <span className="content5-btn"><img src="/static/images/content-b05.png" width="40%" height="80%"/></span>
+                    <span className="content5-btn2"><img src="/static/images/content05.png" width="40%" height="80%"/></span>
+                    </div>
+
+                    <div className="con6-container">      
+                    <span className="content6-btn"><img src="/static/images/content-b06.png" width="40%" height="80%"/></span>
+                    <span className="content6-btn2"><img src="/static/images/content06.png" width="40%" height="80%"/></span>
+                    </div>
+                    </div>
+
+        </div>
                 );
             }
 }
